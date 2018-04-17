ae47483c156e99760fded67669583400e1f27775
Chely Ho <adept_inept@yahoo.com>
2018-04-17 21:16:11 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if params[:name].present?
+      session[:name] = params[:name]
+      redirect_to '/'
+    else
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to '/'
+  end
+end
