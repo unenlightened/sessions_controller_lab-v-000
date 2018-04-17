dcce6529d7614f88b10c9172350a962a454c2874
Chely Ho <adept_inept@yahoo.com>
2018-04-17 21:12:22 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if session[:username] = params[:name]
+    if params[:name].present?
+      session[:username] = params[:name]
       redirect_to '/'
     else
       redirect_to login_path
